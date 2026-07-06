ea3633412f6597b1d5e59df6785a262b16213193
lourdes <142233493+lulu021@users.noreply.github.com>
2026-07-06 17:15:23 -0600

    Add files via upload

---
+++ js/layers.js
@@ -0,0 +1,16 @@
+// =============================
+// CAPAS GEOGRÁFICAS
+// =============================
+
+
+// Grupo de capas del visor
+
+const grupoCapas = {
+
+    delegaciones: null,
+
+    municipios: null,
+
+    estaciones: null
+
+};
