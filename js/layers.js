00423f038feab99a01d933c737647616397d1538
lourdes <142233493+lulu021@users.noreply.github.com>
2026-07-07 13:27:02 -0600

    Add files via upload

---
+++ js/layers.js
@@ -0,0 +1,41 @@
+// ============================
+// CAPAS DEL GEOVISOR
+// ============================
+
+let capaDelegaciones;
+
+// ============================
+// COLORES OFICIALES DELEGACIONES
+// ============================
+
+function obtenerColorDelegacion(nombre){
+
+    const colores = {
+
+        "Delegación Regional I. Atlacomulco":"#f4e4da",
+
+        "Delegación Regional II. Jilotepec":"#eaccc2",
+
+        "Delegación Regional III. Metepec":"#dcc9d9",
+
+        "Delegación Regional IV. Valle de Bravo":"#dee8d7",
+
+        "Delegación Regional V. Tejupilco":"#f6e7b5",
+
+        "Delegación Regional VI. Tenancingo":"#99c3df",
+
+        "Delegación Regional VII. Cuautitlán Izcalli":"#f1bbcd",
+
+        "Delegación Regional VIII. Zumpango":"#f4f5f5",
+
+        "Delegación Regional IX. Teotihuacán":"#c7e2ba",
+
+        "Delegación Regional X. Texcoco":"#ceb9b5",
+
+        "Delegación Regional XI. Amecameca":"#ade2e5"
+
+    };
+
+    return colores[nombre] || "#D9D9D9";
+
+} 
