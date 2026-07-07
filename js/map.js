ca46ccf348e8498a324957549dce4516e34817b0
lourdes <142233493+lulu021@users.noreply.github.com>
2026-07-07 13:26:33 -0600

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,103 @@
+// Crear mapa centrado en Estado de México
+
+const mapa = L.map("map").setView(
+    CONFIG.centroMapa,
+    CONFIG.zoomInicial
+);
+
+
+// ESRI Satélite (mapa principal)
+
+const esri = L.tileLayer(
+
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+
+    {
+        attribution:
+        "Tiles © Esri"
+    }
+
+).addTo(mapa);
+
+
+// OpenStreetMap (mapa secundario)
+
+const osm = L.tileLayer(
+
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+    {
+
+        attribution:
+        "© OpenStreetMap contributors"
+
+    }
+
+);
+
+
+// Control de mapas
+
+const mapasBase = {
+
+    "🛰️ ESRI Satélite": esri,
+
+    "🌎 OpenStreetMap": osm
+
+};
+
+
+L.control.layers(mapasBase).addTo(mapa);
+
+
+// Escala
+
+L.control.scale().addTo(mapa);
+
+// ============================
+// CARGAR DELEGACIONES
+// ============================
+
+fetch("data/delegaciones.geojson")
+    .then(response => response.json())
+    .then(data => {
+
+        capaDelegaciones = L.geoJSON(data, {
+
+            style: function(feature){
+
+                return {
+
+                    color: "#6e6e6e",
+                    weight: 1,
+
+                    fillColor: obtenerColorDelegacion(feature.properties["Delegació"]),
+
+                    fillOpacity: 0.50
+
+                };
+
+            },
+            onEachFeature: function(feature, layer){
+
+                layer.bindPopup(`
+                <div style="min-width:220px">
+                <h3 style="margin-bottom:8px;">
+                🌾 ${feature.properties["Delegació"]}
+                </h3>
+                <b>Región:</b><br>
+                ${feature.properties.Region}
+                <br><br>
+                <b>Municipio:</b><br>
+                ${feature.properties.NOM_MUN}
+                </div>
+
+                `);
+
+            }
+
+        }).addTo(mapa);
+
+        mapa.fitBounds(capaDelegaciones.getBounds());
+
+    });
