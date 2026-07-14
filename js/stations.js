daafbbb63e39bd41a372f4d94be60af09de8f3bb
lourdes <142233493+lulu021@users.noreply.github.com>
2026-07-14 17:17:14 -0600

    Add files via upload

---
+++ js/stations.js
@@ -0,0 +1,51 @@
+// ==========================================
+// CARGA DE ESTACIONES
+// ==========================================
+
+async function cargarEstaciones(){
+
+    const respuesta = await fetch("data/estaciones.json");
+
+    const estaciones = await respuesta.json();
+
+    for (const estacion of estaciones) {
+
+    const marcador = L.marker([
+    estacion.latitud,
+    estacion.longitud
+    ]).addTo(mapa);
+
+    marcador.bindPopup(`
+    <div style="min-width:220px">
+
+        <h3 style="margin:0 0 8px 0;">
+            📍 ${estacion.nombre}
+        </h3>
+
+        <b>🏙 Municipio</b><br>
+        ${estacion.municipio}
+
+        <hr>
+
+        <div id="weather-${estacion.idLocal}">
+            🌡 Actualizando...
+        </div>
+
+        <hr>
+
+        <div style="text-align:right;">
+            <a href="#" onclick="return false;">Ver detalles</a>
+        </div>
+
+    </div>
+    `); 
+    marcador.on("popupopen", async function () {
+
+        console.log("Abriendo estación:", estacion.nombre);
+
+});
+
+}
+
+
+}
