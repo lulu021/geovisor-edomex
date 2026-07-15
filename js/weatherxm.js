ab615b9edb409bbafc768f00a6b4449b6b2d272e
lourdes <142233493+lulu021@users.noreply.github.com>
2026-07-15 13:01:44 -0600

    Add files via upload

---
+++ js/weatherxm.js
@@ -0,0 +1,109 @@
+const WeatherXMService = {
+
+    // Datos obtenidos de la API
+    estaciones: [],
+
+    // Fecha de la última actualización
+    ultimaActualizacion: null,
+
+    // Estado de carga
+    cargado: false,
+
+    // Caché de datos por estación
+    cache: {},
+
+    // ==========================
+    // Obtener estaciones
+    // ==========================
+
+    async obtenerEstaciones(){
+
+        console.log("Servicio WeatherXM listo.");
+
+    },
+
+ // ==========================
+// Obtener datos de una estación
+// ==========================
+
+async obtenerDatos(idEstacion){
+
+    const cacheEstacion = this.cache[idEstacion];
+
+if (cacheEstacion) {
+
+    const tiempoTranscurrido =
+        Date.now() - cacheEstacion.fecha;
+
+    if (tiempoTranscurrido < CONFIG.weatherxm.tiempoActualizacion) {
+
+        console.log("Usando caché:", idEstacion);
+
+        return cacheEstacion.datos;
+
+    }
+
+}
+
+    try{
+
+        const respuesta = await fetch(
+            `${CONFIG.weatherxm.baseUrl}/stations/${idEstacion}/latest`,
+            {
+                headers:{
+                    "X-API-KEY": CONFIG.weatherxm.apiKey
+                }
+            }
+        );
+
+        console.log("HTTP:", respuesta.status);
+
+        if(!respuesta.ok){
+            throw new Error(`HTTP ${respuesta.status}`);
+        }
+        const datos = await respuesta.json();
+
+        const datosAdaptados = {
+
+            temperatura: datos.observation?.temperature ?? null,
+
+            sensacion: datos.observation?.feels_like ?? null,
+
+            humedad: datos.observation?.humidity ?? null,
+
+            viento: datos.observation?.wind_speed ?? null,
+
+            direccionViento: datos.observation?.wind_direction ?? null,
+
+            lluvia: datos.observation?.precipitation_rate ?? null,
+
+            presion: datos.observation?.pressure ?? null,
+
+            actualizacion: datos.observation?.timestamp ?? null,
+
+            estado: datos.health ?? null
+
+        };
+
+        console.log(datosAdaptados);
+
+        // Guardar en caché
+        this.cache[idEstacion] = {
+
+            datos: datosAdaptados,
+
+            fecha: Date.now()
+
+        };
+
+        return datosAdaptados;
+    }
+    catch(error){
+
+        console.error("Error:", error);
+
+    }
+
+}
+
+}; 
